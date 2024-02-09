3a93ebbcea9cd8ee60f787506c181818755cdd4b
Felype Couto <felype_rocha@ecad.org.br>
2024-02-09 19:35:19 -0300

    feat: added data annotations to dtos

--- src/Api.Domain/Dtos/List/ListCreateDto.cs
+++ src/Api.Domain/Dtos/List/ListCreateDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,20 @@ namespace Api.Domain.Dtos.List
 {
     public class ListCreateDto
     {
+        [Required(ErrorMessage = "Necessário informar o id do Usuário")]
         public Guid UserId { get; set; }
+
+        [Required(ErrorMessage = "Necessário informar o título da Lista")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "Mínimo de {2} de máximo de {1} caracteres")]
         public string Title { get; set; }
+
+
+        [Required]
+        [StringLength(10, MinimumLength = 3, ErrorMessage = "Mínimo de {2} de máximo de {1} caracteres")]
         public string Category { get; set; }
 
+        [Required]
+        [MaxLength(5, ErrorMessage = "Necessário ao menos {1} itens")]
         public IEnumerable<ListItemCreateDto> ListItems { get; set; }
     }
 }
