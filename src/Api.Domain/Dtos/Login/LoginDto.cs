cfb76b73e3d0f7a90bfc983244f6181e225c3d48
Felype Couto <felype_rocha@ecad.org.br>
2024-02-11 21:51:22 -0300

    refactor: added data annotations do valid incoming request body

--- src/Api.Domain/Dtos/Login/LoginDto.cs
+++ src/Api.Domain/Dtos/Login/LoginDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,11 @@ namespace Api.Domain.Dtos.Login
 {
     public class LoginDto
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
         public string Password { get; set; }
     }
 }
