eaf6f7fb3fd2f337078cc1b0c712b7e862ed1ac4
Felype Couto <felype_rocha@ecad.org.br>
2024-02-11 21:53:04 -0300

    feat: added handling errors on categories controller

--- src/Api.Application/Controllers/CategoriesController.cs
+++ src/Api.Application/Controllers/CategoriesController.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Api.Domain.Dtos.Category;
 using Api.Domain.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Application.Controllers
@@ -20,18 +22,38 @@ namespace Api.Application.Controllers
             _service = service;
         }
 
+        [Authorize("Bearer")]
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            return Ok(await _service.GetCategories());
+
+            try
+            {
+                return Ok(await _service.GetCategories());
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                throw;
+            }
         }
 
+        [Authorize("Bearer")]
         [HttpPost]
         public async Task<IActionResult> Post(CategoryCreateDto category)
         {
 
+            try
+            {
+                return Ok(await _service.AddCategory(category));
 
-            return Ok(await _service.AddCategory(category));
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                throw;
+            }
+
 
 
         }
